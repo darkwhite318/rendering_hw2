24f11d7caa71191759cf02a67730a4481b8ad56c
darkwhite318 <r05944002@ntu.edu.tw>
2016-11-02 19:48:01 +0800

    still working on parsing file

---
+++ realistic.h
@@ -0,0 +1,47 @@
+
+#if defined(_MSC_VER)
+#pragma once
+#endif
+
+#ifndef PBRT_CAMERAS_REALISTIC_H
+#define PBRT_CAMERAS_REALISTIC_H
+
+#include "camera.h"
+#include "paramset.h"
+#include "film.h"
+
+struct LensSet{
+
+	float radius;
+	float axpos;
+	float n;
+	float aperture;
+
+};
+
+// RealisticCamera Declarations
+class RealisticCamera : public Camera {
+public:
+	// RealisticCamera Public Methods
+	RealisticCamera(const AnimatedTransform &cam2world,
+						float hither, float yon, float sopen,
+						float sclose, float filmdistance, float aperture_diameter, string specfile,
+						float filmdiag, Film *film);
+	float GenerateRay(const CameraSample &sample, Ray *) const;
+  
+private:
+	// RealisticCamera Public Methods
+	float hither;
+	float yon;
+	float filmdistance;
+	float aperture_diameter;
+	float filmdiag;
+
+};
+
+
+RealisticCamera *CreateRealisticCamera(const ParamSet &params,
+        const AnimatedTransform &cam2world, Film *film);
+
+
+#endif	// PBRT_CAMERAS_REALISTIC_H
